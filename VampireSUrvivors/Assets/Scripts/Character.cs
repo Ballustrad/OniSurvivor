ee2d08799e301a693660e142e2b505965f318d02
Ballustrad <ethan.azoulay@e-artsup.net>
2022-11-26 18:55:09 +0100

    siuuuuu

--- VampireSUrvivors/Assets/Scripts/Character.cs
+++ VampireSUrvivors/Assets/Scripts/Character.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    public int maxHp = 1000;
+    public int currentHp = 1000;
 
+    public void TakeDamage(int damage)
+    {
+        currentHp -= damage;
+
+        if (currentHp <= 0)
+        {
+            Debug.Log("Character is dead Game Over");
+        }
+    }
 }
--- VampireSUrvivors/Assets/Scripts/Enemy.cs
+++ VampireSUrvivors/Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,11 +8,13 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] Transform targetDestination;
     GameObject targetGameObject;
+    Character targetCharacter;
     [SerializeField] float speed;
 
     Rigidbody2D rgbd2d;
 
-    [SerializeField] int hp = 4;
+    [SerializeField] int hp = 999;
+    [SerializeField] int damage = 1;
     private void Awake()
     {
         rgbd2d = GetComponent<Rigidbody2D>();
@@ -35,7 +38,12 @@ public class Enemy : MonoBehaviour
 
     private void Attack()
     {
-        //Debug.Log("attacking the character");
+         if (targetCharacter == null)
+        {
+            targetCharacter = targetGameObject.GetComponent<Character>();
+        }
+
+        targetCharacter.TakeDamage(damage);
     }
 
     public void TakeDamage(int damage)
