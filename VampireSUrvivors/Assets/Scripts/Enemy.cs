9eef82c8e8218beee25b14572562f03be4e7e1b6
Ballustrad <ethan.azoulay@e-artsup.net>
2022-11-27 17:56:51 +0100

    items update

--- VampireSUrvivors/Assets/Scripts/Enemy.cs
+++ VampireSUrvivors/Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour, IDamageable
 {
     Transform targetDestination;
     GameObject targetGameObject;
