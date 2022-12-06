0be87f87ad6561c8c49fb8d1fa7a84fff34ee20f
Ballustrad <ethan.azoulay@e-artsup.net>
2022-12-06 10:02:44 +0100

    spawnenemieswave

--- VampireSUrvivors/Assets/Scripts/Entities/EnemiesManager.cs
+++ VampireSUrvivors/Assets/Scripts/Entities/EnemiesManager.cs
@@ -10,24 +10,16 @@ public class EnemiesManager : MonoBehaviour
     [SerializeField] Vector2 spawnArea;
     [SerializeField] float spawnTimer;
      GameObject player;
-    float timer;
+    
 
     private void Start()
     {
         player = GameManager.instance.playerTransform.gameObject;
     }
 
-    private void Update()
-    {
-        timer -= Time.deltaTime;
-        if (timer < 0f)
-        {
-            SpawnEnemy();
-            timer = spawnTimer; 
-        }
-    }
+    
 
-    private void SpawnEnemy()
+    public void SpawnEnemy()
     {
         Vector3 position = GenerateRandomPosition();
 
--- VampireSUrvivors/Assets/Scripts/World/StageData.cs
+++ VampireSUrvivors/Assets/Scripts/World/StageData.cs
@@ -1,14 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-class StageEvent
+[Serializable]
+public class StageEvent
 {
-
+    public float time;
+    public string message;
+    public Enemy enemyToSpawn;
+    public int count;
 }
 
 [CreateAssetMenu]
 public class StageData : ScriptableObject
 {
-    
+    public List<StageEvent> stageEvents;
 }
